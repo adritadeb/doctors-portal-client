2a972f93f4c84dd43428febf49ef3db5beca6f4c
adritadeb <adritad2019@gmail.com>
2022-05-14 22:52:06 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,35 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const menuItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/appointment'>Appointment</Link></li>
+        <li><Link to='/reviews'>Reviews</Link></li>
+        <li><Link to='/contact'>Contact Us</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+    </>
+
     return (
         <div>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
-                    <a className="btn btn-ghost normal-case text-xl">Doctors Portal</a>
-                </div>
-                <div className="navbar-end">
-                    <div className="hidden lg:flex">
-                        <ul className="menu menu-horizontal p-0">
-                            <li><a>Home</a></li>
-                            <li><a>About</a></li>
-                            <li><a>Appointment</a></li>
-                            <li><a>Reviews</a></li>
-                            <li><a>Contact Us</a></li>
-                            <li><a>Login</a></li>
+                    <div className="dropdown">
+                        <label tabindex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuItems}
                         </ul>
                     </div>
+                    <a className="btn btn-ghost normal-case text-xl">Doctors Portal</a>
                 </div>
-                <div className="dropdown">
-                    <label tabindex="0" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
-                        <li><a>About</a></li>
-                        <li><a>Appointment</a></li>
-                        <li><a>Reviews</a></li>
-                        <li><a>Contact Us</a></li>
-                        <li><a>Login</a></li>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        {menuItems}
                     </ul>
                 </div>
             </div>
