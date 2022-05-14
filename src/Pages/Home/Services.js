36a6e8ac39315fe112d03063c5453a19fc9274a7
adritadeb <adritad2019@gmail.com>
2022-05-15 01:48:42 +0600

    services banner done

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -6,20 +6,34 @@ import Service from './Service';
 
 const Services = () => {
     const services = [
-        { id: 1, img: fluoride, title: 'Fluoride Treatment' },
-        { id: 2, img: cavity, title: 'Cavity Filling' },
-        { id: 3, img: whitening, title: 'Teeth Whitening' }
+        {
+            _id: 1,
+            img: fluoride,
+            title: 'Fluoride Treatment'
+        },
+        {
+            _id: 2,
+            img: cavity,
+            title: 'Cavity Filling'
+        },
+        {
+            _id: 3,
+            img: whitening,
+            title: 'Teeth Whitening'
+        }
     ]
 
     return (
-        <div className='px-12'>
+        <div>
             <div className='py-24'>
                 <h3 className='uppercase text-center font-bold text-secondary'>Our Services</h3>
                 <h2 className='text-center text-3xl font-normal'>Services We Provide</h2>
             </div>
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-10'>
                 {
-                    services.map(service => <Service service={service}></Service>)
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}></Service>)
                 }
             </div>
         </div>
